99fe663ef172700133638948805f3d031e92825f
Konstantin Grimashevich <k.grimashevich@gmail.com>
2022-04-09 18:48:59 +0300

    Lesson 5

--- cSharpOOP/Program.cs
+++ cSharpOOP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cSharpOOP.Lesson5;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,24 @@ namespace cSharpOOP
     {
         static void Main(string[] args)
         {
+            Fraction fr1 = new Fraction(7,13);
+            Fraction fr2 = new Fraction(0.5);
+            Console.WriteLine(fr1);
+            Console.WriteLine(fr2);
+            Console.WriteLine($"{fr1} + {fr2} = {fr1 + fr2}");
+            Console.WriteLine($"{fr1} - {fr2} = {fr1 - fr2}");
+            Console.WriteLine($"{fr1} * {fr2} = {fr1 * fr2}");
+            Console.WriteLine($"{fr1} / {fr2} = {fr1 / fr2}");
+            Console.WriteLine($"{fr1} % {fr2} = {fr1 % fr2}");
+            Console.WriteLine($"{fr1} > {fr2} = {fr1 > fr2}");
+            Console.WriteLine($"{fr1} <= {fr2} = {fr1 <= fr2}");
+            Console.WriteLine($"{fr1} == {fr2} = {fr1 == fr2}");
+            Console.WriteLine($"{fr1} != {fr2} = {fr1 != fr2}");
+            Console.WriteLine($"{fr1}++ = {fr1++}");
+            Console.WriteLine($"{fr1}-- = {fr1--}");
+            Console.WriteLine($"float({fr1}) = {(float)fr1}");
+            Console.WriteLine($"double({fr1}) = {(double)fr1}");
+            
         }
     }
 }
