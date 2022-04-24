b4cd8d9bd692a7ea7781f2c9f7e72a8efdf4c7e3
Konstantin Grimashevich <k.grimashevich@gmail.com>
2022-04-24 18:58:29 +0300

    Lesson 7

---
+++ cSharpOOP/Lesson7/IСoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpOOP.Lesson7
+{
+    internal interface IСoder
+    {
+        string Encode(string value);
+        string Decode(string value);
+    }
+}
--- cSharpOOP/Program.cs
+++ cSharpOOP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cSharpOOP.Lesson7;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace cSharpOOP
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("ACoder:\n");
+            var coder = new ACoder();
+            string s1 = "ABCdefАБВгде";
+            string s2 = coder.Encode(s1);
+            string s3 = coder.Decode(s2);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            
+            Console.WriteLine("\nBCoder:\n");
+            coder = new BCoder();
+            s1 = "ABCdefАБВгде";
+            s2 = coder.Encode(s1);
+            s3 = coder.Decode(s2);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
         }
     }
 }
